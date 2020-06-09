2309e5477e1813b8cc759d544a342ad75857512c
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-06-09 21:09:40 +0600

    useReducer example

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,11 +1,21 @@
 import React, { useReducer } from "react";
 
 const initialState = 0;
-const reducer = (state,action) => {
-    /**
-     * reducer() accepts two values & returns one value
-     */
-    
+const reducer = (state, action) => {
+  /**
+   * reducer() accepts two values & returns one value
+   * action - is like an instrunction to the reducer function
+   */
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
 };
 const Counter = () => {
   /**
@@ -13,12 +23,15 @@ const Counter = () => {
    * first arg is a function - reducer()
    * second arg is initial state - initialState
    */
-  useReducer(reducer, initialState);
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <button>Increment</button>
-      <button>Decrement</button>
-      <button>Reset</button>
+      <div>
+        <h1>Count: {count}</h1>
+      </div>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
+      <button onClick={() => dispatch("reset")}>Reset</button>
     </div>
   );
 };
