6c110cf87d1215c78f3ae387b879b46f590b7bd8
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-22 22:07:19 +0600

    on submit error - object is not iterable

--- src/components/PatientManagement/PatientManagement.js
+++ src/components/PatientManagement/PatientManagement.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useReducer } from 'react';
 import { patientReducer, patientState } from '../../reducers/patientReducer';
 
 
 const PatientManagement = () => {
+    const nameRef = useRef();
     const [state, dispatch] = useReducer(patientReducer, patientState);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({ type: 'ADD_PATIENT', name: nameRef.current.value, id: state.patients.length + 1 })
+        console.log(nameRef.current.value);
+    }
     return (
         <div>
             <h2>Patient Management : {state.patients.length}</h2>
+            <form onSubmit={handleSubmit}>
+                <input ref={nameRef} />
+            </form>
         </div>
     );
 };
--- src/reducers/patientReducer.js
+++ src/reducers/patientReducer.js
@@ -2,7 +2,11 @@ export const patientState = { patients: [] }
 export const patientReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_PATIENT':
-            return state;
+            const newPatient = {
+                id: action.id,
+                name: action.name
+            }
+            return [...state, newPatient];
         case 'REMOVE_PATIENT':
             return state;
         default:
