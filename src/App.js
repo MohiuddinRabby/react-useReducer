e5bfe0c6c0e618068033bac26187270225798a7c
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-28 12:20:04 +0600

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReducerCount from './components/ReducerCount/ReducerCount';
-import PatientManagement from './components/PatientManagement/PatientManagement';
-
+import React from "react";
+import ReducerCount from "./components/ReducerCount/ReducerCount";
+import PatientManagement from "./components/PatientManagement/PatientManagement";
+import ReducerCountTwo from "./components/ReducerCountTwo/ReducerCountTwo";
 
 const App = () => {
   return (
     <div>
       <ReducerCount></ReducerCount>
       <PatientManagement></PatientManagement>
+      <ReducerCountTwo></ReducerCountTwo>
     </div>
   );
-}
+};
 
 export default App;
