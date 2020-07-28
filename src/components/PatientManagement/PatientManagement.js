8a011ff128c49fc52e9786c383fec46b4f3936f2
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-28 11:16:34 +0600

    useReducer updated-remove added

--- src/components/PatientManagement/PatientManagement.js
+++ src/components/PatientManagement/PatientManagement.js
@@ -1,27 +1,35 @@
-import React, { useRef } from 'react';
-import { useReducer } from 'react';
-import { patientReducer, patientState } from '../../reducers/patientReducer';
-
+import React, { useRef } from "react";
+import { useReducer } from "react";
+import { patientReducer, patientState } from "../../reducers/patientReducer";
 
 const PatientManagement = () => {
-    const nameRef = useRef();
-    const [state, dispatch] = useReducer(patientReducer, patientState);
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch({ type: 'ADD_PATIENT', name: nameRef.current.value, id: state.patients.length + 1 })
-        nameRef.current.value = '';
-    }
-    return (
-        <div>
-            <h2>Patient Management : {state.patients.length}</h2>
-            <form onSubmit={handleSubmit}>
-                <input ref={nameRef} />
-            </form>
-            {
-                state.patients.map(pt => <li>{pt.name}</li>)
-            }
-        </div>
-    );
+  const nameRef = useRef();
+  const [state, dispatch] = useReducer(patientReducer, patientState);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_PATIENT",
+      name: nameRef.current.value,
+      id: state.patients.length + 1,
+    });
+    nameRef.current.value = "";
+  };
+  return (
+    <div>
+      <h2>Patient Management : {state.patients.length}</h2>
+      <form onSubmit={handleSubmit}>
+        <input ref={nameRef} />
+      </form>
+      {state.patients.map((pt) => (
+        <li
+          key={pt.id}
+          onClick={() => dispatch({ type: "REMOVE_PATIENT", id: pt.id })}
+        >
+          {pt.name} <button>X</button>
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default PatientManagement;
--- src/reducers/patientReducer.js
+++ src/reducers/patientReducer.js
@@ -1,16 +1,20 @@
-export const patientState = { patients: [] }
+export const patientState = { patients: [] };
 export const patientReducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD_PATIENT':
-            const newPatient = {
-                id: action.id,
-                name: action.name
-            }
-            const allPatients = [...state.patients, newPatient]
-            return { patients: allPatients };
-        case 'REMOVE_PATIENT':
-            return state;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "ADD_PATIENT":
+      const newPatient = {
+        id: action.id,
+        name: action.name,
+      };
+      const allPatients = [...state.patients, newPatient];
+      return { patients: allPatients };
+    case "REMOVE_PATIENT":
+      const remainingPatients = state.patients.filter(
+        (pt) => pt.id !== action.id
+      );
+      const newState = { patients: remainingPatients };
+      return newState;
+    default:
+      return state;
+  }
+};
