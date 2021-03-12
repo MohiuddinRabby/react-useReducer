65bd4f2924d5108e85a096cfb286f03e8625516b
MohiuddinRabby <rabbymohiuddin@gmail.com>
2021-03-12 23:53:35 +0600

    update-old example removed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from "react";
-import ReducerCount from "./components/ReducerCount/ReducerCount";
-import PatientManagement from "./components/PatientManagement/PatientManagement";
-import ReducerCountTwo from "./components/ReducerCountTwo/ReducerCountTwo";
-import Datafetching from "./components/Datafetching/Datafetching";
-import DatafetchingTwo from "./components/DatafetchingTwo/DatafetchingTwo";
 
 const App = () => {
-  return (
-    <div>
-      <ReducerCount></ReducerCount>
-      <PatientManagement></PatientManagement>
-      <ReducerCountTwo></ReducerCountTwo>
-      <Datafetching></Datafetching>
-      <DatafetchingTwo></DatafetchingTwo>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
